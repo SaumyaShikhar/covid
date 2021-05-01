5631caa1e34cf07ac6afba2ba83f02265ecb5e95
SaumyaShikhar <shikhar.saumya@gmail.com>
2021-05-01 23:40:33 +0530

    Update COWIN Slot Availability (DISTRICT).py

--- COWIN Slot Availability (DISTRICT).py
+++ COWIN Slot Availability (DISTRICT).py
@@ -32,3 +32,25 @@ for INP_DATE in date_str:
                             if(session["vaccine"] != ''):                         
                                 print("\t Vaccine: ", session["vaccine"])
                             print("\n\n")
+                            for INP_DATE in date_str:
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(DIST_ID, INP_DATE)
+    response = requests.get(URL)
+    if response.ok:
+        resp_json = response.json()
+        # print(json.dumps(resp_json, indent = 1))
+        if resp_json["centers"]:
+            for center in resp_json["centers"]:
+                for session in center["sessions"]:
+                    if session["min_age_limit"] <= age:
+                        if(session["available_capacity"]>0):
+                            print("Available on: {}".format(INP_DATE))
+                            print("\t", center["name"])
+                            print("\t", center["block_name"])
+                            print("\t Price: ", center["fee_type"])
+                            print("\t Available Capacity: ", session["available_capacity"])
+                            if(session["vaccine"] != ''):                         
+                                print("\t Vaccine: ", session["vaccine"])
+                            print("\n\n")
+                            count = count + 1
+if count == 0:
+    print("No slots available")
